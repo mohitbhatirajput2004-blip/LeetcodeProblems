58ff0cefd18d7b6f3bbdfa68dda60ade00d00d7b
Mohit Singh <mohitbhatirajput2004@gmail.com>
2026-01-27 18:59:55 +0530

    Time: 13 ms (66.28%) | Memory: 48.8 MB (27.55%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,30 +1,18 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        int[] result = new int[nums.length];
-        int n = nums.length;
-        int newa[] = new int[2*n];
-
-        for(int i = 0 ; i < nums.length ; i++){
-            newa[i] = nums[i];
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        ans[n-1]  = -1;
+        Stack<Integer> st = new Stack<>();
+       for(int i=n-1;i>=0;i--){
+        st.push(arr[i]);
+       }
+        for(int i=n-1;i>=0;i--){
+               while(st.size()>0 && arr[i]>=st.peek()) st.pop();
+               if(st.size()==0) ans[i]=-1;
+               else ans[i]= st.peek();
+               st.push(arr[i]);
         }
-        for(int i = 0 ; i < nums.length ; i++){
-            newa[i + nums.length ] = nums[i];
-        }
- for (int i = 0; i < nums.length; i++) {
-            result[i] = -1; 
-          
-            
-            for (int j = i+1; j < newa.length; j++) {
-                if (nums[i] < newa[j]) {
-                    result[i] = newa[j];
-                    break;
-                }
-                
-            }
-
-        }
-
-
-        return result;
+        return ans;
     }
 }
