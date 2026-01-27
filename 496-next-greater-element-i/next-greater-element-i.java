a056305ba608c90a39f2d081618e76b817f55342
Mohit Singh <mohitbhatirajput2004@gmail.com>
2026-01-27 17:09:04 +0530

    Time: 5 ms (17.94%) | Memory: 46.5 MB (8.42%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-        int[] result = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = -1;
-
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] > nums1[i]) {
-                            result[i] = nums2[k];
-                            break;
-                        }
-                    }
-                    break;
-                }
+        int ans[] = new int[nums1.length];
+        HashMap<Integer , Integer> map = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+        HashMap<Integer , Integer> mapping = new HashMap<>();
+         for(int i = 0;i< nums2.length ; i++ ) mapping.put(nums2[i] , i);
+        for(int i =0 ; i < nums2.length ; i++){
+            while(!s.isEmpty() && nums2[s.peek()] < nums2[i]){
+                map.put(s.pop() , i);
             }
+            s.push(i);
         }
-        return result;
+    
+        for(int i = 0; i< nums1.length ; i++){
+            int x = mapping.get(nums1[i]);
+
+            if(map.containsKey(x)){
+            int ng = map.get(x);
+            ans[i] = nums2[ng];
+            }else{
+                ans[i] = -1;
+            }
+
+        }
+
+    return ans;
     }
 }
