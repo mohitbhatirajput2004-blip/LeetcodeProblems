c51da24a0818b5550675ee8733c8ba7c28c710b3
Mohit Singh <mohitbhatirajput2004@gmail.com>
2026-01-28 16:16:50 +0530

    Update rotate-non-negative-elements.java

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] rotateElements(int[] nums, int k) {
-        // Step 1: collect non-negative elements
+      
         int count = 0;
         for (int num : nums) {
             if (num >= 0) count++;
@@ -15,15 +15,15 @@ class Solution {
         }
         int n=nums.length;
 
-        // Step 2: rotate the array 'posa'
+        
         if(count == 0) return nums;
-        k = k % count; // handle cases where k > count
+        k = k % count; 
          reverse(posa, 0, k - 1);
 
-        // Step 2: reverse remaining n-k elements
+      
         reverse(posa, k, count - 1);
 
-        // Step 3: reverse whole array
+       
         reverse(posa, 0, count - 1);
 
         x =0 ;
@@ -39,7 +39,7 @@ for(int i = 0 ; i < nums.length ; i++){
     return nums;
     }
 
-    // Helper function to reverse part of array
+  
     private void reverse(int[] arr, int start, int end) {
         while (start < end) {
             int temp = arr[start];
