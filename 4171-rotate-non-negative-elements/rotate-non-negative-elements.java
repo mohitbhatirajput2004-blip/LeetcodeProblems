17dedbd912611973e1fb3cb5ea70dc18e3638c7b
Mohit Singh <mohitbhatirajput2004@gmail.com>
2026-01-29 22:45:46 +0530

    Time: 15 ms (94.04%) | Memory: 275.9 MB (9.47%) - LeetSync

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -1,11 +1,10 @@
 class Solution {
-    public int[] rotateElements(int[] nums, int k) {
-      
+    public int[] rotateElements(int[] nums, int k) {     
         int count = 0;
         for (int num : nums) {
-            if (num >= 0) count++;
+            if (num >= 0)
+                count++;
         }
-
         int[] posa = new int[count];
         int x = 0;
         for (int num : nums) {
@@ -13,34 +12,27 @@ class Solution {
                 posa[x++] = num;
             }
         }
-        int n=nums.length;
-
-        
-        if(count == 0) return nums;
+        int n = nums.length;
+        if (count == 0)
+            return nums;
         k = k % count; 
-         reverse(posa, 0, k - 1);
 
-      
+        reverse(posa, 0, k - 1);
         reverse(posa, k, count - 1);
-
-       
         reverse(posa, 0, count - 1);
 
-        x =0 ;
+        x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                nums[i] = posa[x];
+                x++;
+            }
 
-for(int i = 0 ; i < nums.length ; i++){
-        if(nums[i] >= 0 ){
-            nums[i] = posa[x];
-            x++;
         }
-
-    }    
-    
-    return nums;
+        return nums;
     }
 
-  
-    private void reverse(int[] arr, int start, int end) {
+    void reverse(int[] arr, int start, int end) {
         while (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
