2b4fd3fc06cefb914bfee0867ed69e395c498f0f
Mohit Singh <mohitbhatirajput2004@gmail.com>
2026-02-01 15:10:55 +0530

    Time: 4 ms (52.33%) | Memory: 43 MB (64.67%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,43 +1,38 @@
 class Solution {
     public String reorganizeString(String s) {
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->(b.count-a.count));
-        Map<Character,Integer>map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char cc=s.charAt(i);
-            map.put(cc,map.getOrDefault(cc,0)+1);
+        int n = s.length();
+        Map<Character, Integer> mp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+            
+            // Optimization: If any char count exceeds (n+1)/2, it's impossible
+            if (mp.get(c) > (n + 1) / 2) return "";
         }
-        for(char c:map.keySet()){
-            pq.add(new Pair(map.get(c),c));
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (Map.Entry<Character, Integer> e : mp.entrySet()) {
+            pq.add(new int[]{e.getValue(), e.getKey()});
         }
-        StringBuilder sb=new StringBuilder();
-        while(pq.size()>0){
-            Pair maxElem=pq.poll();
-            if(sb.length()>0 && sb.charAt(sb.length()-1)==maxElem.c){
-                if(pq.size()==0){
-                    break;
-                }
-                Pair secTop=pq.poll();
-                sb.append(secTop.c);
-                secTop.count--;
-                if(secTop.count>0){
-                    pq.add(secTop);
-                }
-            }else{
-                sb.append(maxElem.c);
-                maxElem.count--;
-            }
-            if(maxElem.count>0)
-            pq.add(maxElem);
+
+        StringBuilder res = new StringBuilder();
+        while (pq.size() > 1) {
+            // Pull the two most frequent characters to ensure they are separated
+            int[] first = pq.poll();
+            int[] second = pq.poll();
+
+            res.append((char) first[1]);
+            res.append((char) second[1]);
+
+            if (--first[0] > 0) pq.add(first);
+            if (--second[0] > 0) pq.add(second);
         }
-        if(sb.length()==s.length()) return sb.toString();
-        return "";
-    }
-}
-class Pair{
-    int count;
-    char c;
-    Pair(int count,char c){
-        this.count=count;
-        this.c=c;
+
+        // If there's one character left, add it to the end
+        if (!pq.isEmpty()) {
+            res.append((char) pq.poll()[1]);
+        }
+
+        return res.toString();
     }
 }
